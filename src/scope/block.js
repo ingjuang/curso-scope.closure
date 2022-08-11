aafc95651e4d9435a64a54e5ed18c86c65349370
Juan Daviod Gomez <juan.gomez@contratista.puntoscolombia.com>
2022-08-10 22:51:50 -0500

    .

--- src/scope/block.js
+++ src/scope/block.js
@@ -3,10 +3,10 @@ function fruits(){
         var fruit1 = 'Apple'; //function scope
         let fruit2 = 'Kiwi'; //block scope
         const fruit3 = 'Banana' //block scope
-        console.log(fruit2);
-        console.log(fruit3);
+        console.log(fruit2); //se accede sólo desde el bloque {}
+        console.log(fruit3); //se accede sólo desde el bloque {}
     }
-    console.log(fruit1);
+    console.log(fruit1); //se puede acceder desde la función
 }
 
 fruits();
